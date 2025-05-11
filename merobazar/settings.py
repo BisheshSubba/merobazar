ff5cebea31e305006d7ae9502f3ed9b934714e39
BisheshSubba <135198307+BisheshSubba@users.noreply.github.com>
2025-05-11 21:03:10 +0545

    Set ALLOWED_HOSTS for Render deployment

--- merobazar/settings.py
+++ merobazar/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['merobazar-jnrt.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
