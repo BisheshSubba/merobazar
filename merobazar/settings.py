1d7f03b6b7f74f87fee01b89e55f8720f2f6064c
BisheshSubba <135198307+BisheshSubba@users.noreply.github.com>
2025-08-28 21:08:23 +0545

    Added a new template file

--- merobazar/settings.py
+++ merobazar/settings.py
@@ -129,7 +129,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "bisheshs255@gmail.com"
+EMAIL_HOST_USER = "merobazar6@gmail.com"
 EMAIL_HOST_PASSWORD = "odqiaqfvknisjtez" 
 
 # Internationalization
